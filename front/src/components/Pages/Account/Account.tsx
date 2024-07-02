5d8d25ef7edcde87160b5c0f70964391609d7d9c
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2024-07-02 16:31:08 +0500

    fx

--- front/src/components/Pages/Account/Account.tsx
+++ front/src/components/Pages/Account/Account.tsx
@@ -2,15 +2,10 @@ import { NavBottom } from "../NavBottom/NavBottom";
 import { Region } from "../Region/Region";
 import { Register } from "../Register/Register";
 import { ZayavkiLinks } from "../ZayavkiLinks/ZayavkiLink";
-import { useQuery } from "@tanstack/react-query";
-import { RegType, useAuth } from "../../Hooks/useAuth";
+
 import { LogType } from "../../Hooks/useAuth";
-import { queryClient } from "../../../queryClient";
-import { Loader } from "../../UI/Loader";
-import ErrorPage from "../ErrorPage/ErrorPage";
-import Auth from "../Auth/Auth/Auth";
+
 import { Navigation } from "../Navigation/Navigation";
-// import { Routes, Route } from "react-router-dom";
 const Account: React.FC<LogType> = ({ username, password }) => {
 	return (
 		<>
