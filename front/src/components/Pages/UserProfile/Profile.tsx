f15903b432367ad8b93276837a00f21ce22390ef
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2024-07-11 14:45:15 +0500

    fx

--- front/src/components/Pages/UserProfile/Profile.tsx
+++ front/src/components/Pages/UserProfile/Profile.tsx
@@ -2,14 +2,13 @@ import "./Profile.css";
 import { Link } from "react-router-dom";
 import { Prev } from "../../UI/PrevLink/Prev";
 import { Avatar } from "@mui/material";
-import { useQuery, useMutation } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { queryClient } from "../../../queryClient";
 import { useAuth } from "../../API/Hooks/useAuth";
 import { Loader } from "../../UI/Loader";
 import ErrorPage from "../ErrorPage/ErrorPage";
 import { useState } from "react";
 import defUphoto from "../../../assets/ErrorPage.jpg";
-import { setPhoto } from "../../API/Hooks/setPhoto";
 const Profile: React.FC = () => {
 	const [defaultName, setDefaultName] = useState<string | undefined>("Кимки");
 	const { getMe } = useAuth();
@@ -21,36 +20,6 @@ const Profile: React.FC = () => {
 		queryClient,
 	);
 
-	/**
-	 * Отрпавка запроса на добавление фото
-	 */
-
-	const [uPhoto, setUphoto] = useState<File | undefined>();
-	const handleGetImg = (e: React.ChangeEvent<HTMLImageElement>) => {
-		const file = e.target.files?.[0];
-		if (file) {
-			setUphoto(file);
-		}
-	};
-
-	const photoMutate = useMutation(
-		{
-			mutationFn: () =>
-				setPhoto({ username: uQuery.data?.username, file: uPhoto }),
-			onSuccess: () =>
-				queryClient.invalidateQueries({ queryKey: ["users", "me"] }),
-		},
-		queryClient,
-	);
-
-	const handleAddFile = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const file = e.target.files?.[0];
-		setUphoto(file);
-		if (uPhoto) {
-			photoMutate.mutateAsync();
-			console.log(`first`);
-		}
-	};
 	switch (uQuery.status) {
 		case "pending":
 			return <Loader />;
@@ -58,47 +27,47 @@ const Profile: React.FC = () => {
 			return <ErrorPage />;
 	}
 
+	//
 	if (uQuery.status === "success") {
 		return (
 			//Надо реализовать возможность выгрузки фото
 			<>
 				<section className="sections">
-					<h2>PHOTO</h2>
-					<form onSubmit={() => photoMutate.mutate()}>
-						<input onChange={handleAddFile} type="file" />
-						<button>Отрпавить</button>
-					</form>
-					<h2>PHOTO</h2>
 					<div className="container">
-						<div className="profile_header">
-							<Prev className="profile prev" to={"#"}>
-								Назад
-							</Prev>
-							<h2 className="sections__title profile_title">
-								Салом,{" "}
-								{uQuery.data?.username ? uQuery.data?.username : defaultName}
-							</h2>
-							<div className="profile_avatar">
-								<Avatar className="nav_user-log" alt="user">
-									<img
-										style={{ maxWidth: "40px", minHeight: "40px" }}
-										src={uQuery.data?.photo ? uQuery.data?.photo : defUphoto}
-										alt=""
-									/>
-								</Avatar>
+						<div className="profile_content km__content">
+							<div className="profile_header">
+								<Prev className="profile prev" to={"#"}>
+									Назад
+								</Prev>
+								<h2 className="sections__title profile_title">
+									Салом,{" "}
+									{uQuery.data?.username ? uQuery.data?.username : defaultName}
+								</h2>
+								<div className="profile_avatar">
+									<Avatar className="nav_user-log" alt="user">
+										<img
+											style={{ maxWidth: "40px", minHeight: "40px" }}
+											src={uQuery.data?.photo ? uQuery.data?.photo : defUphoto}
+											alt=""
+										/>
+									</Avatar>
+								</div>
 							</div>
-						</div>
-						<div className="profile__content km__content">
-							<div className="left_side-content">
-								<ul className="list">
-									<li className="list_item">
-										<Link to={"/"} className="item_link">
-											Линк
-										</Link>
-									</li>
-								</ul>
+							<div className="uInfo_content">
+								<div className="uLeft_content">
+									<ul className="list">
+										<li className="list_item">
+											<Link to={"/"} className="item_link">
+												Линк
+											</Link>
+										</li>
+									</ul>
+								</div>
+								<div className="uCenter_info">
+									{" "}
+									<h2>PHOTO || INFO || ANYTHING</h2>
+								</div>
 							</div>
-							<div className="center_content">center</div>
 						</div>
 					</div>
 				</section>
