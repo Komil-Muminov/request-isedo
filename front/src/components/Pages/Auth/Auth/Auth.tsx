a2567652a274b3dfd5896df729b6a95002a37690
AMIRTJK <jsharipovamir@gmail.com>
2024-07-19 17:43:50 +0500

    improved design of the login page

--- front/src/components/Pages/Auth/Auth/Auth.tsx
+++ front/src/components/Pages/Auth/Auth/Auth.tsx
@@ -3,42 +3,51 @@ import React from "react";
 import Registration from "../Registration.tsx/Registarion";
 import Authorization from "../Authorization/Authorization";
 const Auth: React.FC = () => {
-	const [authType, setAuthType] = React.useState<string>("registration");
+  const [authType, setAuthType] = React.useState<string>("registration");
 
-	const handleClick = () => {
-		setAuthType((prev) =>
-			prev === "registration" ? "authorization" : "registration",
-		);
-	};
+  const handleClick = (state: string) => {
+    setAuthType(state);
+  };
 
-	return (
-		<section className="sections auth__section">
-			<div className="container">
-				<div className="auth_box">
-					<div className="auth__content km__content">
-						<div className="auth_render">
-							{authType === "registration" ? (
-								<Registration />
-							) : (
-								<Authorization />
-							)}
-						</div>
-						<div className="auth_logic">
-							<span className="auth_rechange" onClick={handleClick}>
-								{authType === "registration"
-									? "У вас есть аккаунт ? "
-									: `У вас нет аккаунта ?`}
-							</span>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section className="sections auth__section">
+      <div className="container">
+        <div className="auth_box">
+          <div className="wrapper-buttons">
+            <button
+              onClick={() => handleClick("registration")}
+              className={`${
+                authType === "registration" ? "btn-reg" : "btn-auth"
+              }`}
+            >
+              Зарегистрироваться
+            </button>
+            <button
+              onClick={() => handleClick("authorization")}
+              className={`${
+                authType === "registration" ? "btn-auth" : "btn-reg"
+              }`}
+            >
+              Войти
+            </button>
+          </div>
+          <div className="auth__content">
+            <div className="auth_render">
+              {authType === "registration" ? (
+                <Registration />
+              ) : (
+                <Authorization />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Auth;
 
 {
-	/*  */
+  /*  */
 }
--- front/src/components/Pages/Auth/Registration.tsx/Registarion.tsx
+++ front/src/components/Pages/Auth/Registration.tsx/Registarion.tsx
@@ -10,96 +10,110 @@ import ErrorPage from "../../ErrorPage/ErrorPage";
 import Authorization from "../Authorization/Authorization";
 
 const Registration = () => {
-	// Надо доработать
-	const {
-		register,
-		handleSubmit,
-		getValues,
-		formState: { errors },
-	} = useForm<RegType>({
-		defaultValues: {
-			username: "",
-			password: "",
-			role: "",
-		},
-	});
+  // Надо доработать
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm<RegType>({
+    defaultValues: {
+      username: "",
+      password: "",
+      role: "",
+    },
+  });
 
-	const { regMe } = useAuth();
-	const data: RegType = getValues();
-	console.log(data);
-	const regMutate = useMutation(
-		{
-			mutationFn: () => regMe(data),
-			onError: () => console.log(`No`),
-		},
-		queryClient,
-	);
-	const onSubmit = () => {
-		regMutate.mutate();
-	};
+  const { regMe } = useAuth();
+  const data: RegType = getValues();
+  console.log(data);
+  const regMutate = useMutation(
+    {
+      mutationFn: () => regMe(data),
+      onError: () => console.log(`No`),
+    },
+    queryClient
+  );
+  const onSubmit = () => {
+    regMutate.mutate();
+  };
 
-	switch (regMutate.status) {
-		case "pending":
-			return <Loader />;
-		case "success":
-			return <Authorization />;
-		case "error":
-			return <ErrorPage />;
-	}
+  switch (regMutate.status) {
+    case "pending":
+      return <Loader />;
+    case "success":
+      return <Authorization />;
+    case "error":
+      return <ErrorPage />;
+  }
 
-	return (
-		<>
-			<form className="form form__content" onSubmit={handleSubmit(onSubmit)}>
-				<input
-					{...register("username", {
-						required: {
-							value: true,
-							message: "это поле объязательно к заполенению",
-						},
-						minLength: {
-							value: 2,
-							message: "Имя пользователя должен содержать минимум 5 символов",
-						},
-					})}
-					placeholder="username"
-					className="inp reg_inp"
-					name="username"
-				/>
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form_form-content">
+          <h3>Министерство финансов</h3>
+          <input
+            {...register("username", {
+              required: {
+                value: true,
+                message: "это поле объязательно к заполенению",
+              },
+              minLength: {
+                value: 2,
+                message: "Имя пользователя должен содержать минимум 5 символов",
+              },
+            })}
+            placeholder="Логин"
+            className="reg_inp"
+            name="username"
+          />
 
-				<span className="form_errors-text">
-					{errors?.username && errors.username.message}
-				</span>
+          <span className="form_errors-text">
+            {errors?.username && errors.username.message}
+          </span>
 
-				<input
-					{...register("password", {
-						required: {
-							value: true,
-							message: "Заполните поле password",
-						},
-						minLength: {
-							value: 2,
-							message: "password должен содержать минимум 5 символов",
-						},
-					})}
-					placeholder="password"
-					className="inp reg_inp"
-					type="password"
-					name="password"
-				/>
+          <input
+            {...register("password", {
+              required: {
+                value: true,
+                message: "Заполните поле password",
+              },
+              minLength: {
+                value: 2,
+                message: "password должен содержать минимум 5 символов",
+              },
+            })}
+            placeholder="Пароль"
+            className="reg_inp"
+            type="password"
+            name="password"
+          />
 
-				<select {...register("role")} className="inp reg_inp-select" id="role">
-					<option value="user">User</option>
-					<option value="admin">Admin</option>
-				</select>
-				<span className="form_errors-text">
-					{errors?.password && errors.password.message}
-				</span>
-				<ButtonKM isLoading={regMutate.isPending} type="btn submit_btn">
-					Зарегистрироваться
-				</ButtonKM>
-			</form>
-		</>
-	);
+          <select
+            {...register("role")}
+            className="reg_inp reg_inp-select"
+            id="role"
+          >
+            <option className="reg_inp-option" value="">
+              Тип пользователя
+            </option>
+            <option className="reg_inp-option" value="user">
+              User
+            </option>
+            <option className="reg_inp-option" value="admin">
+              Admin
+            </option>
+          </select>
+          <span className="form_errors-text">
+            {errors?.password && errors.password.message}
+          </span>
+        </div>
+        <ButtonKM isLoading={regMutate.isPending} type="btn submit_btn">
+          Зарегистрироваться
+        </ButtonKM>
+      </form>
+    </>
+  );
 };
 
 export default Registration;
