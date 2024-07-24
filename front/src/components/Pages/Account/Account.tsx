03f1065f62dd0e26e52cc27de15e5303f74b6e9e
AMIRTJK <jsharipovamir@gmail.com>
2024-07-24 11:21:53 +0500

    add notification

--- front/src/components/Pages/Account/Account.tsx
+++ front/src/components/Pages/Account/Account.tsx
@@ -2,17 +2,17 @@ import { NavBottom } from "../NavBottom/NavBottom";
 import { Region } from "../Region/Region";
 import { Register } from "../Register/Register";
 import { ZayavkiLinks } from "../ZayavkiLinks/ZayavkiLink";
-import { Routes, Route, Link } from "react-router-dom";
+// import { Routes, Route, Link } from "react-router-dom";
 
 const Account: React.FC = () => {
-	return (
-		<>
-			<NavBottom />
-			<ZayavkiLinks />
-			<Region />
-			<Register />
-		</>
-	);
+  return (
+    <>
+      <NavBottom />
+      <ZayavkiLinks />
+      <Region />
+      <Register />
+    </>
+  );
 };
 
 export default Account;
