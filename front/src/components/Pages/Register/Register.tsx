0395286d0b08324544fc334aa218975af5167e56
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2024-07-25 08:35:29 +0500

    fx

--- front/src/components/Pages/Register/Register.tsx
+++ front/src/components/Pages/Register/Register.tsx
@@ -3,13 +3,9 @@ import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { Link, useNavigate } from "react-router-dom";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { queryClient } from "../../../queryClient";
 import { getRqsts, GetRqsts } from "../../API/GetRqsts";
-import { Logout } from "@mui/icons-material";
-import { logout } from "../../API/Logout";
-import Auth from "../Auth/Auth/Auth";
-
 export const Register: React.FC = () => {
 	const getRqsQuery = useQuery(
 		{
@@ -22,10 +18,9 @@ export const Register: React.FC = () => {
 	const [rqstsData, setRqstsData] = useState<GetRqsts[]>([]);
 	useEffect(() => {
 		if (getRqsQuery.status === "success") {
-			// setData((prev) => [...prev, ...getRqsQuery.data]);
 			setRqstsData(getRqsQuery.data);
 		}
-	}, [getRqsQuery.data, setRqstsData]);
+	}, [getRqsQuery.status === "success", rqstsData]);
 
 	const rows = rqstsData.map((e) => {
 		return {
@@ -73,8 +68,8 @@ export const Register: React.FC = () => {
 			<section className="sections register__section">
 				<div className="container">
 					<div className="register__content km__content">
-						<p className="km__info-text">* Блок с фильтрами</p>
-						<p className="km__info-text">* РЕЕСТР ---</p>
+						{/* <p className="km__info-text">* Блок с фильтрами</p>
+						<p className="km__info-text">* РЕЕСТР ---</p> */}
 						<Link
 							to={"/addrequest"}
 							className="btn addrequest_btn"
