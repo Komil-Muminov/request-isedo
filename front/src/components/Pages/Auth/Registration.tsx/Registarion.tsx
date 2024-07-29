fc15dcf7e6de0dbfdc837936d068379c2747328f
AMIRTJK <jsharipovamir@gmail.com>
2024-07-29 11:10:49 +0500

    fixed

--- front/src/components/Pages/Auth/Registration.tsx/Registarion.tsx
+++ front/src/components/Pages/Auth/Registration.tsx/Registarion.tsx
@@ -10,48 +10,47 @@ import Authorization from "../Authorization/Authorization";
 import InputAuth from "../../../UI/InputAuth/InputAuth";
 
 const Registration = () => {
-	// Надо доработать
-	const {
-		register,
-		handleSubmit,
-		getValues,
-		watch,
-		formState: { errors },
-	} = useForm<RegType>({
-		defaultValues: {
-			username: "",
-			password: "",
-			uType: "",
-			fullName: "",
-			number: "",
-			role: "",
-			tax: "",
-			email: "",
-		},
-	});
+  // Надо доработать
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    watch,
+    formState: { errors },
+  } = useForm<RegType>({
+    defaultValues: {
+      username: "",
+      password: "",
+      uType: "",
+      fullName: "",
+      number: "",
+      role: "",
+      tax: "",
+      email: "",
+    },
+  });
 
-	const uType = watch("uType");
-	const { regMe } = useAuth();
-	const data: RegType = getValues();
-	console.log(data);
-	const regMutate = useMutation(
-		{
-			mutationFn: () => regMe(data),
-			onSuccess: () => queryClient.invalidateQueries({ queryKey: ["rqsts"] }),
-			onError: () => console.log(`No`),
-		},
-		queryClient,
-	);
-	const onSubmit = () => {
-		regMutate.mutate();
-	};
+  const uType = watch("uType");
+  const { regMe } = useAuth();
+  const data: RegType = getValues();
+  console.log(data);
+  const regMutate = useMutation(
+    {
+      mutationFn: () => regMe(data),
+      onSuccess: () => queryClient.invalidateQueries({ queryKey: ["rqsts"] }),
+      onError: () => console.log(`No`),
+    },
+    queryClient
+  );
+  const onSubmit = () => {
+    regMutate.mutate();
+  };
 
-	switch (regMutate.status) {
-		case "success":
-			return <Authorization />;
-	}
+  switch (regMutate.status) {
+    case "success":
+      return <Authorization />;
+  }
 
-<<<<<<< HEAD
   return (
     <>
       <form className="form-reg" onSubmit={handleSubmit(onSubmit)}>
@@ -80,64 +79,27 @@ const Registration = () => {
               <InputAuth
                 register={register}
                 inputName="username"
-                inputPlaceholder="Логин"
+                inputPlaceholder="Логин рег"
                 requiredMessage="Это поле объязательно к заполенению"
                 minLengthMessage="Логин должен содержать минимум 9 символов"
                 inputType="number"
                 kind="reg_inp"
               />
-=======
-	return (
-		<>
-			<form className="form-reg" onSubmit={handleSubmit(onSubmit)}>
-				<div className="form-reg_form-content">
-					<h3>Министерство финансов</h3>
-					<select
-						{...register("uType")}
-						className="reg_inp reg_inp-select"
-						id="uType"
-					>
-						<option className="reg_inp-option" value="">
-							Тип пользователя
-						</option>
-						<option className="reg_inp-option" value="mfrt">
-							МФРТ
-						</option>
-						<option className="reg_inp-option" value="bo">
-							БО
-						</option>
-						<option className="reg_inp-option" value="ko">
-							КО
-						</option>
-					</select>
-					{uType == "bo" ? (
-						<>
-							<InputAuth
-								register={register}
-								inputName="username"
-								inputPlaceholder="Логин рег"
-								requiredMessage="Это поле объязательно к заполенению"
-								minLengthMessage="Логин должен содержать минимум 9 символов"
-								inputType="number"
-								kind="reg_inp"
-							/>
->>>>>>> 93e128cb73a7f6c1185e230614dcf522b49c843a
 
-							<span className="form_errors-text">
-								{errors?.username && errors.username.message}
-							</span>
+              <span className="form_errors-text">
+                {errors?.username && errors.username.message}
+              </span>
 
-							<InputAuth
-								register={register}
-								inputName="password"
-								inputPlaceholder="Пароль"
-								requiredMessage="Заполните поле password"
-								minLengthMessage="Пароль должен содержать минимум 5 символов"
-								inputType="password"
-								kind="reg_inp"
-							/>
+              <InputAuth
+                register={register}
+                inputName="password"
+                inputPlaceholder="Пароль"
+                requiredMessage="Заполните поле password"
+                minLengthMessage="Пароль должен содержать минимум 5 символов"
+                inputType="password"
+                kind="reg_inp"
+              />
 
-<<<<<<< HEAD
               <span className="form_errors-text">
                 {errors?.password && errors.password.message}
               </span>
@@ -227,77 +189,6 @@ const Registration = () => {
       {console.log(data)}
     </>
   );
-=======
-							<InputAuth
-								register={register}
-								inputName="fullName"
-								inputPlaceholder="fullName"
-								requiredMessage="Это поле объязательно к заполенению"
-								minLengthMessage="Логин должен содержать минимум 9 символов"
-								inputType="text"
-								kind="reg_inp"
-							/>
-							<InputAuth
-								register={register}
-								inputName="number"
-								inputPlaceholder="number"
-								requiredMessage="Это поле объязательно к заполенению"
-								minLengthMessage="number должен содержать минимум 9 символов"
-								inputType="text"
-								kind="reg_inp"
-							/>
-							<InputAuth
-								register={register}
-								inputName="role"
-								inputPlaceholder="role"
-								requiredMessage="Это поле объязательно к заполенению"
-								minLengthMessage="role должен содержать минимум 9 символов"
-								inputType="text"
-								kind="reg_inp"
-							/>
-							<InputAuth
-								register={register}
-								inputName="tax"
-								inputPlaceholder="tax"
-								requiredMessage="Это поле объязательно к заполенению"
-								minLengthMessage="tax должен содержать минимум 9 символов"
-								inputType="text"
-								kind="reg_inp"
-							/>
-							<InputAuth
-								register={register}
-								inputName="email"
-								inputPlaceholder="email"
-								requiredMessage="Это поле объязательно к заполенению"
-								minLengthMessage="email должен содержать минимум 9 символов"
-								inputType="text"
-								kind="reg_inp"
-							/>
-
-							<span className="form_errors-text">
-								{errors?.username && errors.username.message}
-							</span>
-
-							<span className="form_errors-text">
-								{errors?.password && errors.password.message}
-							</span>
-						</>
-					) : (
-						<>НЕТ</>
-					)}
-				</div>
-				<ButtonKM
-					disabled={regMutate.isPending}
-					isLoading={regMutate.isPending}
-					type="submit"
-				>
-					Зарегистрироваться
-				</ButtonKM>
-			</form>
-			{console.log(data)}
-		</>
-	);
->>>>>>> 93e128cb73a7f6c1185e230614dcf522b49c843a
 };
 
 export default Registration;
