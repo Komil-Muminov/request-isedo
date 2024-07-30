169e66ebbdf6d55dfb298567f609af35dc6b3796
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2024-07-30 17:28:45 +0500

    fx

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -10,10 +10,12 @@ import "./components/Pages/Auth/Auth/Auth.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./queryClient.ts";
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // Предоставляет контекст для использования react-query во всем приложении. queryClient управляет состоянием кэширования запросов.
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </QueryClientProvider>
+	// Предоставляет контекст для использования react-query во всем приложении. queryClient управляет состоянием кэширования запросов.
+	// React StrictMode
+	<QueryClientProvider client={queryClient}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</QueryClientProvider>,
+	// React StrictMode
 );
