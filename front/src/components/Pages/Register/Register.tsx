c36431d0249f19463cf407fbae34ce7f6c8aa81d
AMIRTJK <jsharipovamir@gmail.com>
2024-07-31 16:11:08 +0500

    fixed

--- front/src/components/Pages/Register/Register.tsx
+++ front/src/components/Pages/Register/Register.tsx
@@ -6,98 +6,109 @@ import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { queryClient } from "../../../queryClient";
 import { getRqsts, GetRqsts } from "../../API/GetRqsts";
+
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+
 export const Register: React.FC = () => {
-	const getRqsQuery = useQuery(
-		{
-			queryFn: () => getRqsts(),
-			queryKey: ["requests"],
-		},
-		queryClient,
-	);
+  const getRqsQuery = useQuery(
+    {
+      queryFn: () => getRqsts(),
+      queryKey: ["requests"],
+    },
+    queryClient
+  );
 
-	const [rqstsData, setRqstsData] = useState<GetRqsts[]>([]);
-	useEffect(() => {
-		if (getRqsQuery.status === "success") {
-			setRqstsData(getRqsQuery.data);
-		}
-	}, [getRqsQuery.status === "success", rqstsData]);
+  const [rqstsData, setRqstsData] = useState<GetRqsts[]>([]);
+  useEffect(() => {
+    if (getRqsQuery.status === "success") {
+      setRqstsData(getRqsQuery.data);
+    }
+  }, [getRqsQuery.status === "success", rqstsData]);
 
-	const rows = rqstsData.map((e) => {
-		return {
-			id: e.id,
-			lastName: e.orgname,
-			firstName: e.accountant,
-			age: e.desc,
-		};
-	});
+  const rows = rqstsData.map((e) => {
+    return {
+      id: e.id,
+      lastName: e.orgname,
+      firstName: e.accountant,
+      age: e.desc,
+    };
+  });
 
-	const columns: GridColDef<(typeof rows)[number]>[] = [
-		{ field: "id", headerName: "ID", width: 90 },
-		{
-			field: "firstName",
-			headerName: "First name",
-			width: 150,
-			editable: false,
-		},
-		{
-			field: "lastName",
-			headerName: "Last name",
-			width: 150,
-			editable: false,
-		},
-		{
-			field: "age",
-			headerName: "Age",
-			type: "number",
-			width: 110,
-			editable: false,
-		},
-		{
-			field: "fullName",
-			headerName: "Full name",
-			description: "This column has a value getter and is not sortable.",
-			sortable: true,
-			width: 160,
-			valueGetter: (value, row) =>
-				`${row.firstName || ""} ${row.lastName || ""}`,
-		},
-	];
+  const columns: GridColDef<(typeof rows)[number]>[] = [
+    { field: "id", headerName: "№", width: 90 },
+    {
+      field: "firstName",
+      headerName: "Дата",
+      width: 150,
+      editable: false,
+    },
+    {
+      field: "lastName",
+      headerName: "Тип",
+      width: 150,
+      editable: false,
+    },
+    {
+      field: "age",
+      headerName: "Исполнитель",
+      type: "number",
+      width: 110,
+      editable: false,
+    },
+    {
+      field: "fullName",
+      headerName: "Регион",
+      description: "This column has a value getter and is not sortable.",
+      sortable: true,
+      width: 160,
+      valueGetter: (value, row) =>
+        `${row.firstName || ""} ${row.lastName || ""}`,
+    },
+  ];
 
-	return (
-		<>
-			<section className="sections register__section">
-				<div className="container">
-					<div className="register__content km__content">
-						{/* <p className="km__info-text">* Блок с фильтрами</p>
+  return (
+    <>
+      <section className="sections register__section">
+        <div className="container">
+          <div className="register__content">
+            {/* <p className="km__info-text">* Блок с фильтрами</p>
 						<p className="km__info-text">* РЕЕСТР ---</p> */}
-						<Link
-							to={"/addrequest"}
-							className="btn addrequest_btn"
-							type="btn submit_btn register_add-btn"
-							onClick={() => console.log(`Переход`)}
-						>
-							Добавить
-						</Link>
-						{/* <Search rows={rows} /> */}
-						<Box sx={{ height: 400, width: "100%" }}>
-							<DataGrid
-								columns={columns}
-								rows={rows}
-								initialState={{
-									pagination: {
-										paginationModel: {
-											pageSize: 5,
-										},
-									},
-								}}
-								pageSizeOptions={[5]}
-								checkboxSelection
-								disableRowSelectionOnClick
-							/>
-						</Box>
-					</div>
-				</div>
-			</section>
-		</>
-	);
+            <Link
+              to={"/addrequest"}
+              className="btn addrequest_btn"
+              type="btn submit_btn register_add-btn"
+              onClick={() => console.log(`Переход`)}
+            >
+              <AddCircleOutlineOutlinedIcon />
+              <p> Добавить</p>
+            </Link>
+            {/* <Search rows={rows} /> */}
+            <Box
+              sx={{
+                height: 400,
+                width: "100%",
+                backgroundColor: "#fff",
+                borderRadius: "40px",
+              }}
+            >
+              <DataGrid
+                columns={columns}
+                rows={rows}
+                initialState={{
+                  pagination: {
+                    paginationModel: {
+                      pageSize: 5,
+                    },
+                  },
+                }}
+                pageSizeOptions={[5]}
+                checkboxSelection
+                disableRowSelectionOnClick
+              />
+            </Box>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
