545f68d9cfcb9f6a77622ed2114c3b8eb054a35a
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2024-08-01 15:35:19 +0500

    fx

--- front/src/components/Pages/NavBottom/NavBottom.tsx
+++ front/src/components/Pages/NavBottom/NavBottom.tsx
@@ -4,23 +4,23 @@ import { Link } from "react-router-dom";
 import { moduls } from "../../API/Data/Moduls/Moduls";
 
 export const NavBottom: React.FC = () => {
-  return (
-    <>
-      <section className="sections navbottom__section">
-        <div className="container">
-          <div className="navbottom__content">
-            <nav className="navbottom__links">
-              {moduls.map((item, index) => {
-                return (
-                  <Link key={index} to="noLink" className="navbottom_item">
-                    {item}
-                  </Link>
-                );
-              })}
-            </nav>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+	return (
+		<>
+			<section className="sections navbottom__section">
+				<div className="container">
+					<div className="navbottom__content">
+						<nav className="navbottom__links">
+							{moduls.map((item, id) => {
+								return (
+									<Link key={id} to="noLink" className="navbottom_item">
+										{item}
+									</Link>
+								);
+							})}
+						</nav>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 };
