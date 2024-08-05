8c8933b82059f38d8446f0a098eb92bbfbc9df2b
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2024-08-05 15:14:28 +0500

    fx

--- front/src/components/UI/InputIdentification/InputIdentification.tsx
+++ front/src/components/UI/InputIdentification/InputIdentification.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { TextField } from "@mui/material";
 
 import { TProps } from "./InputIdentificationType";
--- front/src/components/UI/Loader/Loader.tsx
+++ front/src/components/UI/Loader/Loader.tsx
@@ -2,27 +2,27 @@ import { useState, useEffect } from "react";
 import "./Loader.css";
 
 export const Loader = () => {
-  const [visible, setVisible] = useState(true);
+	const [visible, setVisible] = useState(true);
 
-  // Анимация исчезновения после 3 секунд
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setVisible(false);
-    }, 1000); // Увеличил время анимации исчезновения до 3 секунд
+	// Анимация исчезновения после 3 секунд
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setVisible(false);
+		}, 1000); // Увеличил время анимации исчезновения до 3 секунд
 
-    return () => clearTimeout(timer);
-  }, []);
+		return () => clearTimeout(timer);
+	}, []);
 
-  return (
-    <>
-      {visible && (
-        <div className="loading__container">
-          <div className="loading__content">
-            <div className="loading__spinner"></div>
-            <span className="loading__message">Интизор бошед...</span>
-          </div>
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			{visible && (
+				<div className="loading__container">
+					<div className="loading__content">
+						<div className="loading__spinner"></div>
+						<span className="loading__message">Интизор бошед...</span>
+					</div>
+				</div>
+			)}
+		</>
+	);
 };
