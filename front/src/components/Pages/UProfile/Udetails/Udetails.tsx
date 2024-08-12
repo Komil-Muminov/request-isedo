c6a1d489c2d0ec15b32d4c12382fbc1a9f37e8ba
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2024-08-12 15:45:07 +0500

    fx

--- front/src/components/Pages/UProfile/Udetails/Udetails.tsx
+++ front/src/components/Pages/UProfile/Udetails/Udetails.tsx
@@ -86,7 +86,7 @@ const Udetails = () => {
 				</div>
 			</div>
 			<div className="uright_info">
-				<Ulink className="btn uidentify_link" to="/uProfile/uIdentity">
+				<Ulink className="btn uidentify_link" to="/uprofile/uIdentity">
 					Идентификация
 				</Ulink>
 			</div>
