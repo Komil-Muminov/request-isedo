9c38b1c45b09cd83b97f9c6452d6f613b4bf5067
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2024-08-14 10:32:15 +0500

    fx

--- front/src/components/UI/AddFileRequest/AddFileRequest.tsx
+++ front/src/components/UI/AddFileRequest/AddFileRequest.tsx
@@ -3,23 +3,27 @@ import "./AddFileRequest.css";
 import { TProps } from "../TableRow/TableRowRequestType";
 
 const AddFileRequest = ({ item, handleGetFile }: TProps) => {
-  const [fileName, setFileName] = useState("Выберите файл");
+	const [fileName, setFileName] = useState("Выберите файл");
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0] || null;
-    setFileName(file ? file.name : "Выберите файл");
-    handleGetFile(item.id, file);
-  };
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const file = event.target.files?.[0] || null;
+		setFileName(file ? file.name : "Выберите файл");
+		handleGetFile(item?.id, file);
+	};
 
-  return (
-    <div className="file-input-wrapper">
-      <input id={`file-input-${item.id}`} type="file" onChange={handleChange} />
-      <label htmlFor={`file-input-${item.id}`} className="custom-file-label">
-        <span className="custom-file-input"></span>
-        <span className="file-input-text">{fileName}</span>
-      </label>
-    </div>
-  );
+	return (
+		<div className="file-input-wrapper">
+			<input
+				id={`file-input-${item?.id}`}
+				type="file"
+				onChange={handleChange}
+			/>
+			<label htmlFor={`file-input-${item?.id}`} className="custom-file-label">
+				<span className="custom-file-input"></span>
+				<span className="file-input-text">{fileName}</span>
+			</label>
+		</div>
+	);
 };
 
 export default AddFileRequest;
