2df73b09e52321a7fa454a12514ac848b10b8e47
AMIRTJK <jsharipovamir@gmail.com>
2024-08-21 09:36:31 +0500

    fx

--- front/src/components/UI/Ulinks/ProfileLinks.ts
+++ front/src/components/UI/Ulinks/ProfileLinks.ts
@@ -1,32 +1,32 @@
 export interface UlinkScheme {
-	url: string;
-	label: string;
-	subLinks?: UlinkScheme[]; // Optional property for nested links
+  url: string;
+  label: string;
+  subLinks?: UlinkScheme[]; // Optional property for nested links
 }
 
 export const UlinksProps: UlinkScheme[] = [
-	{
-		url: "/account",
-		label: "Главная страница",
-		subLinks: [
-			{
-				url: `unotify`,
-				label: `Уведомление`,
-			},
+  {
+    url: "/account",
+    label: "Главная страница",
+    subLinks: [
+      {
+        url: `unotify`,
+        label: `Уведомление`,
+      },
 
-			{
-				url: "udetails",
-				label: "Информация о пользователе",
-			},
-		],
-	},
-	{
-		url: "/organization",
-		label: "Организация",
-	},
-	{
-		url: "/account",
-		label: "Реестр заявок",
-		subLinks: [{ url: "unotify", label: "Запросы на добавление организации" }],
-	},
+      {
+        url: "udetails",
+        label: "Информация о пользователе",
+      },
+    ],
+  },
+  {
+    url: "/organization",
+    label: "Организация",
+  },
+  {
+    url: "/account",
+    label: "Заявки",
+    subLinks: [{ url: "unotify", label: "Запросы на добавление организации" }],
+  },
 ];
