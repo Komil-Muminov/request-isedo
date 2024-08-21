c79a974097fd50067491cdefe1c8ba00a774b0df
AMIRTJK <jsharipovamir@gmail.com>
2024-08-21 09:38:45 +0500

    fx class section, add #fff color

--- front/src/components/Pages/Reqshower/Reqshower.tsx
+++ front/src/components/Pages/Reqshower/Reqshower.tsx
@@ -1,41 +1,41 @@
 import { useQuery } from "@tanstack/react-query";
 import { queryClient } from "../../../queryClient";
-import { GetRqsts, getRqsts } from "../../API/GetRqsts";
+import { getRqsts } from "../../API/GetRqsts";
 import { Outlet } from "react-router-dom";
 import "./Reqshower.css";
 const Reqshower = () => {
-	const showReqQuery = useQuery(
-		{
-			queryFn: () => getRqsts(),
-			queryKey: ["getreqbo"],
-		},
-		queryClient,
-	);
+  const showReqQuery = useQuery(
+    {
+      queryFn: () => getRqsts(),
+      queryKey: ["getreqbo"],
+    },
+    queryClient
+  );
 
-	return (
-		<>
-			<div className="reqshower_conten">
-				<h1 className="sections__title">Показать запросы работниками КВД</h1>
-				{/* Надо сделать верстку для данных*/}
-				{showReqQuery.data &&
-					showReqQuery.data?.map((item) => (
-						<div>
-							<p className="req_text">
-								{" "}
-								Наименование организации: {item.orgname}
-							</p>
+  return (
+    <>
+      <div className="reqshower_conten">
+        <h1 className="sections__title">Показать запросы работниками КВД</h1>
+        {/* Надо сделать верстку для данных*/}
+        {showReqQuery.data &&
+          showReqQuery.data?.map((item) => (
+            <div>
+              <p className="req_text">
+                {" "}
+                Наименование организации: {item.orgname}
+              </p>
 
-							<p className="req_text"> ФИО Бухгалтера: {item.accountant}</p>
-							<p className="req_text"> Тип заявки: {item.reqStatus}</p>
-							<p className="req_text"> Cтатус заявки: {item.reqType}</p>
-							<p className="req_text"> Описание заявки: {item.desc}</p>
-						</div>
-					))}
-				{/* Надо сделать верстку для данных*/}
-				<Outlet />
-			</div>
-		</>
-	);
+              <p className="req_text"> ФИО Бухгалтера: {item.accountant}</p>
+              <p className="req_text"> Тип заявки: {item.reqStatus}</p>
+              <p className="req_text"> Cтатус заявки: {item.reqType}</p>
+              <p className="req_text"> Описание заявки: {item.desc}</p>
+            </div>
+          ))}
+        {/* Надо сделать верстку для данных*/}
+        <Outlet />
+      </div>
+    </>
+  );
 };
 
 export default Reqshower;
