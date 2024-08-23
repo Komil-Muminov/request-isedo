adb4d9ae34f9ee4e9ce8dd0d720ea6acda46f69f
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2024-08-23 17:14:01 +0500

    fx

--- front/src/components/Pages/ShowRequest/ShowRequest.tsx
+++ front/src/components/Pages/ShowRequest/ShowRequest.tsx
@@ -4,13 +4,13 @@ import { useNavigate, useParams } from "react-router-dom";
 import { getRqstsById, GetRqstsByIdType } from "../../API/GetRqstsById";
 import { queryClient } from "../../../queryClient";
 import {
-  Button,
-  TextField,
-  Stepper,
-  StepLabel,
-  Step,
-  Typography,
-  IconButton,
+	Button,
+	TextField,
+	Stepper,
+	StepLabel,
+	Step,
+	Typography,
+	IconButton,
 } from "@mui/material";
 import { Select } from "@radix-ui/themes";
 import "../AddRequest/AddRequest.css";
@@ -34,214 +34,215 @@ import TitleDocument from "../../UI/TitleDocument/TitleDocument";
 import WorkSpace from "../../UI/WorkSpace/WorkSpace";
 
 const ShowRequest = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const { id } = useParams();
-  const numericId = parseInt(id || "", 10);
+	const { id } = useParams();
+	const numericId = parseInt(id || "", 10);
 
-  const getRqstsByIdQuery = useQuery(
-    {
-      queryFn: () => getRqstsById(numericId),
-      queryKey: [`request-${numericId}`],
-    },
-    queryClient
-  );
+	const getRqstsByIdQuery = useQuery(
+		{
+			queryFn: () => getRqstsById(numericId),
+			queryKey: [`request-${numericId}`],
+		},
+		queryClient,
+	);
 
-  const [rqstsData, setRqstsData] = useState<GetRqstsByIdType | null>(null);
+	const [rqstsData, setRqstsData] = useState<GetRqstsByIdType | null>(null);
 
-  const { getMe } = useAuth();
-  const uQuery = useQuery(
-    {
-      queryFn: () => getMe(),
-      queryKey: ["users", "me"],
-    },
-    queryClient
-  );
+	const { getMe } = useAuth();
+	const uQuery = useQuery(
+		{
+			queryFn: () => getMe(),
+			queryKey: ["users", "me"],
+		},
+		queryClient,
+	);
 
-  const [uinfo, setUinfo] = useState<GetMeType | null>(null);
-  // const [expanded, setExpanded] = useState<number | false>(false);
+	const [uinfo, setUinfo] = useState<GetMeType | null>(null);
+	// const [expanded, setExpanded] = useState<number | false>(false);
 
-  useEffect(() => {
-    if (uQuery.status === "success") {
-      setUinfo(uQuery.data);
-    }
-  }, [uQuery.status, uQuery.data]);
+	useEffect(() => {
+		if (uQuery.status === "success") {
+			setUinfo(uQuery.data);
+		}
+	}, [uQuery.status, uQuery.data]);
 
-  if (uQuery.status === "pending") return <Loader />;
-  if (uQuery.status === "error") {
-    console.log(uQuery.error);
-    return null;
-  }
+	if (uQuery.status === "pending") return <Loader />;
+	if (uQuery.status === "error") {
+		console.log(uQuery.error);
+		return null;
+	}
 
-  const steps =
-    uinfo?.uType === "kvd"
-      ? stepsOfKvd
-      : uinfo?.uType === "bo"
-      ? stepsOfBo
-      : [];
+	const steps =
+		uinfo?.uType === "kvd"
+			? stepsOfKvd
+			: uinfo?.uType === "bo"
+			? stepsOfBo
+			: [];
 
-  useEffect(() => {
-    if (getRqstsByIdQuery.status === "success") {
-      setRqstsData(getRqstsByIdQuery.data);
-    } else if (getRqstsByIdQuery.status === "error") {
-      console.error(getRqstsByIdQuery.error);
-    }
-  }, [getRqstsByIdQuery]);
+	useEffect(() => {
+		if (getRqstsByIdQuery.status === "success") {
+			setRqstsData(getRqstsByIdQuery.data);
+		} else if (getRqstsByIdQuery.status === "error") {
+			console.error(getRqstsByIdQuery.error);
+		}
+	}, [getRqstsByIdQuery]);
 
-  if (getRqstsByIdQuery.status === "pending") {
-    return <p>Loading...</p>;
-  }
+	if (getRqstsByIdQuery.status === "pending") {
+		return <p>Loading...</p>;
+	}
 
-  console.log(rqstsData);
+	console.log(rqstsData);
 
-  return (
-    <main className="show-content">
-      <div className="container">
-        <div className="details-steps">
-          <TitleDocument title="Новая заявка" />
-        </div>
-        <section className="show-steps">
-          <div className="panel-control">
-            <ButtonPanelControl
-              goBack={() => navigate(-1)}
-              icon={
-                <ArrowBackIosIcon
-                  sx={{ fontSize: "18px", fontWeight: "bold" }}
-                />
-              }
-              text="Назад"
-            />
-            <div className="wrapper-buttons">
-              <ButtonPanelControl
-                icon={
-                  <SaveIcon sx={{ fontSize: "18px", fontWeight: "bold" }} />
-                }
-                text="Сохранить"
-              />
-              <ButtonPanelControl
-                icon={
-                  <UnsubscribeIcon
-                    sx={{ fontSize: "18px", fontWeight: "bold" }}
-                  />
-                }
-                text="Отправить на резолюцию"
-              />
-              <ButtonPanelControl
-                icon={
-                  <CancelIcon sx={{ fontSize: "18px", fontWeight: "bold" }} />
-                }
-                text="Отклонить"
-              />
-              <ButtonPanelControl
-                icon={
-                  <DoneIcon sx={{ fontSize: "18px", fontWeight: "bold" }} />
-                }
-                text="Завершить"
-              />
-            </div>
-          </div>
-          <Stepper
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              padding: "20px",
-              width: "100%",
-              // Убираем линии между шагами
-              "& .MuiStepConnector-root": {
-                display: "none",
-              },
-            }}
-            activeStep={rqstsData?.stepCode}
-            alternativeLabel
-          >
-            {steps.map((e, index) => (
-              <Step
-                key={index}
-                sx={{
-                  display: "flex",
-                  flex: "1",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  position: "relative",
-                  "&:first-of-type": {
-                    flex: "0 0 auto", // первый шаг фиксированный
-                    marginLeft: 0,
-                  },
-                  "&:last-of-type": {
-                    flex: "0 0 auto", // последний шаг фиксированный
-                    marginRight: 0,
-                  },
-                }}
-              >
-                <StepLabel>
-                  <p className="step-header">{e.stepName}</p>
-                  <p>{e.initiators}</p>
-                </StepLabel>
-              </Step>
-            ))}
-          </Stepper>
-        </section>
-        <section className="details-request">
-          <div className="title">
-            <p>Детали заявки</p>
-          </div>
-          <div className="form_content">
-            <form className="inputs-list">
-              <TextField
-                type="text"
-                id="orgname"
-                className="request_inp"
-                value={rqstsData?.fullName || ""}
-                label="ФИО"
-              />
-              <TextField
-                type="text"
-                className="request_inp"
-                label="Должность"
-                value={rqstsData?.role || ""}
-              />
-              <TextField
-                type="text"
-                className="request_inp"
-                label="Контанты"
-                value={rqstsData?.phone || ""}
-              />
-              <TextField
-                id="email"
-                type="text"
-                className="request_inp"
-                label="E-mail адрес"
-                value={rqstsData?.email || ""}
-              />
-              <TextField
-                id="tax"
-                type="text"
-                className="request_inp"
-                label="ИНН"
-                value={rqstsData?.tax || ""}
-              />
-              <TextField
-                id="orgTax"
-                type="text"
-                className="request_inp"
-                label="ИНН"
-                value={rqstsData?.orgTax || ""}
-              />
-              <TextField
-                id="orgName"
-                type="text"
-                className="request_inp"
-                label="Организация"
-                value={rqstsData?.orgName || ""}
-              />
-            </form>
-          </div>
-        </section>
-        {/* Рабочее пространство */}
-        <WorkSpace />
-      </div>
-    </main>
-  );
+	return (
+		<main className="show-content">
+			<div className="container">
+				<div className="details-steps">
+					<TitleDocument title="Новая заявка" />
+				</div>
+				<section className="show-steps">
+					<div className="panel-control">
+						<ButtonPanelControl
+							goBack={() => navigate(-1)}
+							icon={
+								<ArrowBackIosIcon
+									sx={{ fontSize: "18px", fontWeight: "bold" }}
+								/>
+							}
+							text="Назад"
+						/>
+						<div className="wrapper-buttons">
+							<ButtonPanelControl
+								icon={
+									<SaveIcon sx={{ fontSize: "18px", fontWeight: "bold" }} />
+								}
+								text="Сохранить"
+							/>
+							<ButtonPanelControl
+								icon={
+									<UnsubscribeIcon
+										sx={{ fontSize: "18px", fontWeight: "bold" }}
+									/>
+								}
+								text="Отправить на резолюцию"
+							/>
+							<ButtonPanelControl
+								icon={
+									<CancelIcon sx={{ fontSize: "18px", fontWeight: "bold" }} />
+								}
+								text="Отклонить"
+							/>
+							<ButtonPanelControl
+								icon={
+									<DoneIcon sx={{ fontSize: "18px", fontWeight: "bold" }} />
+								}
+								text="Завершить"
+							/>
+						</div>
+					</div>
+					<Stepper
+						sx={{
+							display: "flex",
+							justifyContent: "space-between",
+							padding: "20px",
+							width: "100%",
+							// Убираем линии между шагами
+							"& .MuiStepConnector-root": {
+								display: "none",
+							},
+						}}
+						activeStep={rqstsData?.stepCode}
+						alternativeLabel
+					>
+						{steps.map((e, index) => (
+							<Step
+								key={index}
+								sx={{
+									display: "flex",
+									flex: "1",
+									justifyContent: "center",
+									alignItems: "center",
+									position: "relative",
+									"&:first-of-type": {
+										flex: "0 0 auto", // первый шаг фиксированный
+										marginLeft: 0,
+									},
+									"&:last-of-type": {
+										flex: "0 0 auto", // последний шаг фиксированный
+										marginRight: 0,
+									},
+								}}
+							>
+								<StepLabel>
+									<p className="step-header">{e.stepName}</p>
+									<p>{e.initiators}</p>
+								</StepLabel>
+							</Step>
+						))}
+					</Stepper>
+				</section>
+				<section className="details-request">
+					<div className="title">
+						<p>Детали заявки</p>
+					</div>
+					<div className="form_content">
+						<form className="inputs-list">
+							<TextField
+								type="text"
+								id="orgname"
+								className="request_inp"
+								value={rqstsData?.fullName || ""}
+								label="ФИО"
+							/>
+							<TextField
+								type="text"
+								className="request_inp"
+								label="Должность"
+								value={rqstsData?.role || ""}
+							/>
+							<TextField
+								type="text"
+								className="request_inp"
+								label="Контанты"
+								value={rqstsData?.phone || ""}
+							/>
+							<TextField
+								id="email"
+								type="text"
+								className="request_inp"
+								label="E-mail адрес"
+								value={rqstsData?.email || ""}
+							/>
+							<TextField
+								id="tax"
+								type="text"
+								className="request_inp"
+								label="ИНН"
+								value={rqstsData?.tax || ""}
+							/>
+							<TextField
+								id="orgTax"
+								type="text"
+								className="request_inp"
+								label="ИНН"
+								value={rqstsData?.orgTax || ""}
+							/>
+							<TextField
+								id="orgName"
+								type="text"
+								className="request_inp"
+								label="Организация"
+								value={rqstsData?.orgName || ""}
+							/>
+						</form>
+					</div>
+				</section>
+				{/* СОздать компонент для показа файлов которые отрпвились из addRequest */}
+				{/* Рабочее пространство */}
+				<WorkSpace />
+			</div>
+		</main>
+	);
 };
 
 export default ShowRequest;
