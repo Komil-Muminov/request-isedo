f057fed5e2e8bace6d5c58aa6756042c5880efb0
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2024-08-29 17:07:45 +0500

    fx

---
+++ front/src/components/UI/Notify/Notify.tsx
@@ -0,0 +1,12 @@
+// import { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+const Notify: React.FC = () => {
+	return (
+		<>
+			<div className="notify">
+				<h2 className="sections__title">Notify</h2>
+			</div>
+		</>
+	);
+};
+export default Notify;
