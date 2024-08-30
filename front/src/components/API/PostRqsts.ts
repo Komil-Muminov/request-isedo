51e6b5ab2ed2a74e7806b00cc8de6fa43af14a38
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2024-08-30 15:26:53 +0500

    fx

--- front/src/components/API/PostRqsts.ts
+++ front/src/components/API/PostRqsts.ts
@@ -1,41 +1,43 @@
 interface FileType {
-  number: number;
-  fileName: string;
+	number: number;
+	fileName: string;
 }
 
 export interface PostRqstScheme {
-  fullName: string;
-  role: string;
-  phone: string;
-  email: string;
-  tax: string;
-  orgTax: string;
-  orgName: string;
-  reqType: string;
-  stepCode: number;
-  dateTime: string;
-  files: FileType[];
+	fullName: string;
+	role: string;
+	phone: string;
+	email: string;
+	tax: string;
+	orgTax: string;
+	orgName: string;
+	reqType: string;
+	stepCode: number;
+	dateTime: string;
+	files: FileType[];
 }
 
 const token = localStorage.getItem("token");
 
 // Нет useEffect-а, соответственно инвалидации не происходит и GetRqsts не обновляется, нужно форматировать под React Query
+
+// Добавить новые поля и функционал KM
 export const postRequest = async (newData: PostRqstScheme): Promise<any> => {
-  try {
-    const response = await fetch(`http://localhost:3000/requests`, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newData),
-    });
-    if (!response.ok) {
-      throw new Error(`Error on getRequest`);
-    }
-    return response.json();
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
-  }
+	try {
+		const response = await fetch(`http://localhost:3000/requests`, {
+			method: "POST",
+			headers: {
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(newData),
+		});
+		if (!response.ok) {
+			throw new Error(`Error on getRequest`);
+		}
+		return response.json();
+	} catch (error) {
+		console.error("Error:", error);
+		throw error;
+	}
 };
