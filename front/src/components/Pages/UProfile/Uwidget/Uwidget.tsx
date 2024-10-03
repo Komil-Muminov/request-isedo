290a47dae694baed5a3afcbd356e490d911b137b
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2024-10-03 10:03:37 +0500

    fx

--- front/src/components/Pages/UProfile/Uwidget/Uwidget.tsx
+++ front/src/components/Pages/UProfile/Uwidget/Uwidget.tsx
@@ -6,29 +6,21 @@ import Uchartshort from "../Uchart/Uchartshort/Uchartshort";
 export interface UwidgetProps {
 	children?: React.ReactNode;
 	title?: string | number;
-	desc: string | number;
-	kind: string | undefined;
+	desc?: string | number;
+	kind?: string | undefined;
 	isLoading?: boolean;
 	disabled?: boolean;
 	onClick?: (e: React.SyntheticEvent<HTMLElement>) => void;
 }
 
 export const Uwidget: React.FC<UwidgetProps> = ({
-	children,
-	title,
-	desc,
 	kind,
 	onClick,
 }: UwidgetProps) => {
 	return (
 		<>
-			<div className="uwidget__content uwidget__style">
-				{/* <h3 className={`uwidget__title ${kind}`}>
-						{title || `Загаловок виджета`}
-					</h3> */}
+			<div className={`uwidget__content uwidget__style ${kind}`}>
 				<Uchartshort />
-				{/* <span className="uwidget__numinfo">{}</span> */}
-				{/* {children || <LoaderPoints />} */}
 				<Button onClick={onClick} className={`uwidget__btn ${kind}`}>
 					Показать полную информацию{" "}
 				</Button>
