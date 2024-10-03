43fdd71ebcc584e545575a79bc0ac787dea638d8
AMIRTJK <jsharipovamir@gmail.com>
2024-10-03 10:52:58 +0500

    fx

--- front/src/components/UI/WorkSpace/DepartmentCustomer/Installing a certificate/InstallCertificate.tsx
+++ front/src/components/UI/WorkSpace/DepartmentCustomer/Installing a certificate/InstallCertificate.tsx
@@ -1,76 +1,136 @@
 import ButtonPanelControl from "../../../ButtonPanelControl/ButtonPanelControl";
 import "./InstallCertificate.css";
 
-import GppBadIcon from "@mui/icons-material/GppBad";
+import GppGoodIcon from "@mui/icons-material/GppGood";
 import CardMembershipIcon from "@mui/icons-material/CardMembership";
 import { TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { TCertificates } from "../../../../API/GetCertificates";
+import { postCertificates } from "../../../../API/PostCertificates";
 
 const InstallCertificate = () => {
+  const {
+    register,
+    // Записывает все стейты в массив
+    handleSubmit,
+    // Функция dirtyFields возвращает true или false в зависимости от того, было ли изменено поле "Название организации".
+    // Поле для бухгалтера становится доступным только если поле "Название организации" было изменено.
+    watch,
+    setValue,
+    getValues,
+    formState: { dirtyFields },
+  } = useForm<TCertificates>({
+    defaultValues: {
+      userName: "",
+      userTax: "",
+      userPhone: "",
+      role: "",
+      orgName: "",
+      orgTax: "",
+      orgPhone: "",
+      region: "",
+      address: "",
+    },
+  });
+
+  const onSubmit = (data: TCertificates) => {
+    const getDate = new Date();
+
+    const day = String(getDate.getDate()).padStart(2, "0");
+    const month = String(getDate.getMonth() + 1).padStart(2, "0");
+    const year = getDate.getFullYear();
+
+    const dateFrom = `${day}.${month}.${year}`;
+    const dateTo = `${day + 1}.${month}.${year + 1}`;
+
+    const updateReqData = {
+      ...data,
+      userId: "",
+      serialNumber: "200000dd1f63274b121773decc00000000dd1f",
+      validFrom: dateFrom,
+      validTo: dateTo,
+      statusCode: 5,
+    };
+
+    // postCertificates.mutate(updateReqData);
+  };
+
   return (
     <div className="certificate-content">
       <div className="panel-control-certificate-revocation">
         <div className="certificates-revocation-title">
           <CardMembershipIcon />
-          <p>Установка сертификата </p>
+          <p>Выдача сертификата </p>
         </div>
+        <ButtonPanelControl
+          icon={<GppGoodIcon sx={{ fontSize: "18px", fontWeight: "bold" }} />}
+          text="Выдать"
+          handleSubmit={handleSubmit(onSubmit)}
+        />
       </div>
       <div className="inputs-list install-certificate-inputs-list">
         <TextField
-          // {...register("fullName")}
+          {...register("userName")}
           type="text"
-          id="fullName"
+          id="userName"
           className="request_inp"
-          // KM
           label="ФИО"
         />
         <TextField
-          // {...register("email")}
-          id="email"
+          {...register("userTax")}
+          id="userTax"
           type="text"
           className="request_inp"
           label="ИНН пользователя"
         />
         <TextField
-          // {...register("tax")}
-          id="tax"
+          {...register("userPhone")}
+          id="userPhone"
           type="text"
           className="request_inp"
-          label="Организация"
+          label="Номер телефон пользователя"
         />
         <TextField
-          // {...register("phone")}
-          id="phone"
-          type="text"
-          className="request_inp"
-          label="ИНН Организации"
-        />
-        <TextField
-          // {...register("passport")}
-          id="passport"
+          {...register("role")}
+          id="role"
           type="text"
           className="request_inp"
           label="Должность"
         />
         <TextField
-          // {...register("role")}
-          id="role"
+          {...register("orgName")}
+          id="orgName"
+          type="text"
+          className="request_inp"
+          label="Организация"
+        />
+        <TextField
+          {...register("orgTax")}
+          id="orgTax"
+          type="text"
+          className="request_inp"
+          label="ИНН Организации"
+        />
+        <TextField
+          {...register("orgPhone")}
+          id="orgPhone"
+          type="text"
+          className="request_inp"
+          label="Номер телефон организации"
+        />
+        <TextField
+          {...register("region")}
+          id="region"
           type="text"
           className="request_inp"
           label="Регион"
         />
         <TextField
-          // {...register("role")}
-          id="role"
+          {...register("address")}
+          id="address"
           type="text"
           className="request_inp"
           label="Адрес"
-        />
-        <TextField
-          // {...register("role")}
-          id="role"
-          type="text"
-          className="request_inp"
-          label="Номер телефона"
         />
       </div>
     </div>
