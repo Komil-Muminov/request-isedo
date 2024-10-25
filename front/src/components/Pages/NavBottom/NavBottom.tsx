a735739a16205c4b0bcb3921f8ad0de19c3d5254
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2024-10-25 08:20:18 +0500

    fx

--- front/src/components/Pages/NavBottom/NavBottom.tsx
+++ front/src/components/Pages/NavBottom/NavBottom.tsx
@@ -2,29 +2,32 @@ import "./NavBottom.css";
 import { Link } from "react-router-dom";
 
 import { moduls } from "../../API/Data/Moduls/Moduls";
-
+import ArticleIcon from "@mui/icons-material/Article";
 export const NavBottom: React.FC = () => {
-  return (
-    <>
-      <section className="sections navbottom__section">
-        <div className="container">
-          <div className="navbottom__content">
-            <nav className="navbottom__links">
-              {moduls.map((item) => {
-                return (
-                  <Link
-                    key={item.id}
-                    to={item.route}
-                    className="navbottom_item"
-                  >
-                    {item.name}
-                  </Link>
-                );
-              })}
-            </nav>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+	return (
+		<>
+			<section className="sections navbottom__section">
+				<div className="container">
+					<div className="navbottom__content">
+						<nav className="navbottom__links">
+							{moduls.map((item) => {
+								return (
+									<Link
+										key={item.id}
+										to={item.route}
+										className="navbottom_item"
+									>
+										<div className="navbottom__moduls-icons">
+											{item.name}
+											<ArticleIcon />
+										</div>
+									</Link>
+								);
+							})}
+						</nav>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 };
