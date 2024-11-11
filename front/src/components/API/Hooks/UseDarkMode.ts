dba059713029266926cdd7786bb695f7863a7808
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2024-11-11 16:25:29 +0500

    fx

--- front/src/components/API/Hooks/UseDarkMode.ts
+++ front/src/components/API/Hooks/UseDarkMode.ts
@@ -10,11 +10,14 @@ export const useDarkMode = () => {
 	useEffect(() => {
 		// profile_left;
 		const profileSection = document.querySelector(".profile");
+		const navigationSection = document.querySelector(".navigation__section");
 
 		if (isDarkMode) {
 			profileSection?.classList.add("dark-mode");
+			navigationSection?.classList.add("dark-mode");
 		} else {
 			profileSection?.classList.remove("dark-mode");
+			navigationSection?.classList.remove("dark-mode");
 		}
 		localStorage.setItem("darkMode", JSON.stringify(isDarkMode));
 	}, [isDarkMode]);
