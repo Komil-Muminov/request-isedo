70c6d1862f185918fa8250de428b07d88820efd3
AMIRTJK <jsharipovamir@gmail.com>
2024-11-18 16:37:58 +0500

    create Certificate Issuance Type

--- front/src/components/Pages/AddRequest/Types of Requests/Change of chief accountant and manager/ChangeChiefAccountantAndManager.tsx
+++ front/src/components/Pages/AddRequest/Types of Requests/Change of chief accountant and manager/ChangeChiefAccountantAndManager.tsx
@@ -14,8 +14,9 @@ import {
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 
-const ChangeManagement = ({
+const ChangeChiefAccountantAndManager = ({
   currentUser,
+  currentManager,
   handleGetFile,
   fileInfo,
   handleFileUploadedStatus,
@@ -29,9 +30,10 @@ const ChangeManagement = ({
   return (
     <>
       <section className="old-accountant">
-        <TitleDocument title="Прошлый руководитель" />
+        <TitleDocument title="Прошлый руководитель и главный бухгалтер" />
         <div className="wrapper-cards">
           <UserOrOrganizationCard
+            currentManager={currentManager}
             currentUser={currentUser}
             title="Карточка пользователя"
             newFileService={<FileService handleGetFile={handleGetFile} />}
@@ -48,6 +50,14 @@ const ChangeManagement = ({
                     <HighlightOffIcon sx={{ color: "red" }} />
                   )}
                   <p>{fileInfo[0]?.name}</p>
+                </li>
+                <li>
+                  {fileUploadedStatus ? (
+                    <CheckCircleOutlineIcon sx={{ color: "green" }} />
+                  ) : (
+                    <HighlightOffIcon sx={{ color: "red" }} />
+                  )}
+                  <p>{fileInfo[3]?.name}</p>
                 </li>
               </ul>
             }
@@ -168,8 +178,111 @@ const ChangeManagement = ({
           </div>
         </div>
       </section>
+      <section className="new-accountant">
+        <TitleDocument title="Новый главный бухгалтер" />
+        <div className="wrapper-cards wrapper-cards-padding">
+          <div className="inputs-list">
+            <TextField
+              {...register("fullName")}
+              type="text"
+              id="fullName"
+              className="request_inp"
+              // KM
+              label="ФИО"
+            />
+            <TextField
+              {...register("tax")}
+              id="tax"
+              type="text"
+              className="request_inp"
+              label="ИНН"
+            />
+            <TextField
+              {...register("phone")}
+              id="phone"
+              type="text"
+              className="request_inp"
+              label="Номер телефона"
+            />
+            <TextField
+              {...register("email")}
+              id="email"
+              type="text"
+              className="request_inp"
+              label="E-mail адрес"
+            />
+            <TextField
+              {...register("passport")}
+              id="passport"
+              type="text"
+              className="request_inp"
+              label="Серия и номер паспорта"
+            />
+            <TextField
+              {...register("role")}
+              id="role"
+              type="text"
+              className="request_inp"
+              label="Должность"
+            />
+            {/* test commit  */}
+            <Box className="request_inp" sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">
+                  Наличие токена
+                </InputLabel>
+                <Select
+                  {...register("token")}
+                  labelId="demo-simple-select-label"
+                  id="token"
+                  label="token"
+                >
+                  <MenuItem value="Есть">Есть</MenuItem>
+                  <MenuItem value="Нет">Нет</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+            <Box className="request_inp" sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">
+                  Наличие логина iMoF
+                </InputLabel>
+                <Select
+                  {...register("loginImof")}
+                  labelId="demo-simple-select-label"
+                  id="loginImof"
+                  label="loginImof"
+                >
+                  <MenuItem value="Есть">Есть</MenuItem>
+                  <MenuItem value="Нет">Нет</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+            <FileService />
+            <ul className="required-documents">
+              <p>Необходимые документы:</p>
+              <li>
+                {secondFileStatus ? (
+                  <CheckCircleOutlineIcon sx={{ color: "green" }} />
+                ) : (
+                  <HighlightOffIcon sx={{ color: "red" }} />
+                )}
+                <p>{fileInfo[4]?.name}</p>
+              </li>
+              <li>
+                {thirdFileStatus ? (
+                  <CheckCircleOutlineIcon sx={{ color: "green" }} />
+                ) : (
+                  <HighlightOffIcon sx={{ color: "red" }} />
+                )}
+                <p>{fileInfo[2]?.name}</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
 
-export default ChangeManagement;
+export default ChangeChiefAccountantAndManager;
